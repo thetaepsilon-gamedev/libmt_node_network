f13d304f53d20ab5b408123be6fe5180a63fcd74
thetaepsilon-gamedev <thetaepsilon@noreply.users.github.com>
2017-11-01 13:42:37 +0000

    external dependencies: pull in mkfnexploder() from libmthelpers

--- external-dependencies.lua
+++ external-dependencies.lua
@@ -1,3 +1,4 @@
 local libmthelpers = modns.get("com.github.thetaepsilon.minetest.libmthelpers")
 _mod.util.increment_counter = libmthelpers.stats.increment_counter
 _mod.new.queue = libmthelpers.datastructs.new.queue
+_mod.util.mkfnexploder = libmthelpers.check.mkfnexploder
