161aac7d4d56506763ea07a278555a7db7b02599
thetaepsilon-gamedev <thetaepsilon@noreply.users.github.com>
2017-11-03 12:29:14 +0000

    external dependencies: import mkassert from libmthelpers

--- external-dependencies.lua
+++ external-dependencies.lua
@@ -2,3 +2,4 @@ local libmthelpers = modns.get("com.github.thetaepsilon.minetest.libmthelpers")
 _mod.util.increment_counter = libmthelpers.stats.increment_counter
 _mod.new.queue = libmthelpers.datastructs.new.queue
 _mod.util.mkfnexploder = libmthelpers.check.mkfnexploder
+_mod.util.mkassert = libmthelpers.check.mkassert
