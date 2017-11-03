e9a80e532e1755d208281e6b836de6fe3acc04d7
thetaepsilon-gamedev <thetaepsilon@noreply.users.github.com>
2017-11-03 13:13:21 +0000

    external dependencies: import center_on_node() from libmthelpers

--- external-dependencies.lua
+++ external-dependencies.lua
@@ -3,3 +3,4 @@ _mod.util.increment_counter = libmthelpers.stats.increment_counter
 _mod.new.queue = libmthelpers.datastructs.new.queue
 _mod.util.mkfnexploder = libmthelpers.check.mkfnexploder
 _mod.util.mkassert = libmthelpers.check.mkassert
+_mod.util.center_on_node = libmthelpers.playerpos.center_on_node
