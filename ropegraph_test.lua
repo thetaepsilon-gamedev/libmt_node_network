86fb398e7709e41b4e433a855fbe308a1086eb50
thetaepsilon-gamedev <thetaepsilon@noreply.users.github.com>
2017-11-19 15:39:20 +0000

    ropegraph tests: add refcount zero and opposite-side follow testing

--- ropegraph_test.lua
+++ ropegraph_test.lua
@@ -1,5 +1,6 @@
 local checkers = mtrequire("com.github.thetaepsilon.minetest.libmthelpers.check")
 local mkassert = checkers.mkassert
+local listequal = checkers.listequaltest
 
 local m = _mod.modules.ropegraph
 
@@ -42,6 +43,22 @@ m.test = function()
 	successors = g:successor(1)
 	assert(size == 1, "successor list should now have one entry, actual="..size)
 	assert(successors[1] == 2, "successor group should have ID 2")
+
+	-- *now* if we delete vertex c's link, the rope between group 1 and 2 should close
+	g:update("foo", "c", 1, {}, {})
+	successors = g:successor(1)
+	emptycheck(successors, "successor list after removing a and c links (size="..#successors..")")
+
+	-- re-add a with links to two groups now.
+	g:update("foo", "a", 1, {b="bar", c="baz"}, {b=2, c=3})
+	successors = g:successor(1)
+	assert(listequal(successors, {2, 3}), "successor list should point to groups 2 and 3")
+
+	-- destroy b from the "opposite side".
+	-- we should be left with 1 pointing to 3 and vice versa.
+	g:update("bar", "b", 2, {}, {})
+	assert(listequal(g:successor(1), {3}), "group 1 should point to 3 only after removal of b")
+	assert(listequal(g:successor(3), {1}), "group 3 should point to 1")
 
 	return "self-tests completed successfully"
 end
