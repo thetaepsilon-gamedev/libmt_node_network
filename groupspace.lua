85fb725d44396e68a9b79af1e22f66fa0f159e94
thetaepsilon-gamedev <thetaepsilon@noreply.users.github.com>
2017-12-02 14:08:05 +0000

    groupspace.lua: convert to explicit hashing model

--- groupspace.lua
+++ groupspace.lua
@@ -2,8 +2,10 @@
 Similar to vertex spaces, but written with different assumptions in mind.
 Instead of whole graphs where adding link between them indiscriminately merges them,
 vertexes belong to groups which may be subsets of a full interconnected graph.
-The successor function understood by the search algorithm and vertex space remains,
+
+The successor function understood by the search algorithm remains,
 but it is possible that connections between vertices span between groups.
+As usual, successor returns a table mapping from hashes to values for each vertex.
 
 That said, there are still some rules involved.
 In a given group space, there is a 1:1 mapping to/from a given tracked vertex and it's group at all times.
@@ -24,17 +26,30 @@ In most cases, unless a group splits apart, the higher-level graph doesn't need 
 yielding amortised O(1) performance.
 ]]
 
+--[[
+self = {
+	ropegraph = {},	-- see ropegraph.lua
+	maptogroup = {},	-- actually guardedmap from libmthelpers datastructs.
+		-- keys in here are hashes and the values are the group tables.
+		-- groups are passed around by value,
+		-- so callers can already use this to obtain refs to other groups using the ropegraph.
+}
+]]
+local guardedmap = mtrequire("com.github.thetaepsilon.minetest.libmthelpers.datastructs.guardedmap")
 
 
-local update = function(self, vertex)
+
+local whichgroup = function(self, vhash) return self.maptogroup:get(vhash) end
+
+
+
+local update = function(self, vertex, vhash)
 	-- firstly remove any existing information about this vertex
 	-- this should clear existing group mappings etc...
-	local vhash = self.hasher(vertex)
 	self:clearvertex(vhash)
 
 	-- ..so that here we can add the vertex as if new.
-	local successors = successor(vertex)
-	successors = toset(successors)
+	local successors = self.successor(vertex, vhash)
 
 	-- we want to see if any groups touching this vertex has room left.
 	-- if so, add the vertex to that group directly.
@@ -44,14 +59,13 @@ local update = function(self, vertex)
 	local foundgroup = nil
 	local touchingvertices = {}
 	local touchinggroups = {}
-	for successor in successors:iterator() do
-		local shash = self.hasher(successor)
+	for shash, successor in pairs(successors) do
 		local group = self:getgroup(shash)
 		-- argh, why does lua not have continue for loops!?
 		if group then
 			local canfit = (group:size() < self.grouplimit)
 			if not foundgroup and canfit then
-				self:addtogroup(group, hash)
+				self:addtogroup(group, shash, successor)
 				foundgroup = group
 			else
 				touchingvertices[shash] = successor
@@ -64,7 +78,7 @@ local update = function(self, vertex)
 	-- if none of the successors were groups with room left,
 	-- then create a new one and add the vertex to it.
 	-- the touching groups will all have been recorded above.
-	foundgroup = self:newgroupwith(vhash)
+	foundgroup = self:newgroupwith(vertex, vhash)
 
 	-- update the list of groups that this vertex touches.
 	self.ropegraph:update(vertex, vhash, foundgroup, touchingvertices, touchinggroups)
