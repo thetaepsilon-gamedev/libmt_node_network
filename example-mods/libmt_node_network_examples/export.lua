df91c49c3980791ad56975b1d842b816bc3c6d04
thetaepsilon-gamedev <thetaepsilon@noreply.users.github.com>
2017-12-02 14:08:33 +0000

    nuke old example mods

--- example-mods/libmt_node_network_examples/export.lua
+++
@@ -1,4 +0,0 @@
-return {
-	node_virus = _mod.modules.node_virus,
-	demonet = _mod.modules.demo_network,
-}
--- example-mods/libmt_node_network_examples/main.lua
+++
@@ -1,8 +0,0 @@
-local modules = {}
-
-dofile(_mod.modpath.."external-dependencies.lua")
-
-modules.node_virus = dofile(_mod.modpath.."node-virus.lua")
-modules.demo_network = dofile(_mod.modpath.."demo-network.lua")
-
-_mod.modules = modules
