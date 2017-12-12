3e909bfd307d16d635cc98654d3ef546c068e36c
thetaepsilon-gamedev <thetaepsilon@noreply.users.github.com>
2017-12-12 11:49:58 +0000

    external dependencies: remove unused center_on_node()

--- external-dependencies.lua
+++ external-dependencies.lua
@@ -3,7 +3,6 @@ _mod.util.increment_counter = libmthelpers.stats.increment_counter
 _mod.new.queue = libmthelpers.datastructs.new.queue
 _mod.util.mkfnexploder = libmthelpers.check.mkfnexploder
 _mod.util.mkassert = libmthelpers.check.mkassert
-_mod.util.center_on_node = libmthelpers.playerpos.center_on_node
 _mod.util.table_get_single = libmthelpers.tableutils.getsingle
 _mod.util.arraytomap = libmthelpers.tableutils.arraytomap
 _mod.util.mkprofiler = libmthelpers.profiling.create_profiler
